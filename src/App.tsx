c7bceef85aad9ba7440e5e13c6b6a8fa10eebd14
rebootedCoder2 <oborimoon@gmail.com>
2024-02-01 00:17:59 +0900

    styled component- adapting& extending

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,22 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
+const Box = styled.div<{ bgColor: string }>`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
-const Text = styled.span`
-  color: white;
-`;
+
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box bgColor="teal" />
+      <Box bgColor="orange" />
+      <Circle bgColor="tomato" />
     </Father>
   );
 }
