ad0613895686ce52a01be60ca5ebac4397a77ef1
rebootedCoder2 <oborimoon@gmail.com>
2024-02-01 09:37:46 +0900

    setup for crypto cracker

--- src/App.tsx
+++ src/App.tsx
@@ -13,42 +13,9 @@ const Wrapper = styled.div`
   height: 100vh;
   justify-content: center;
   align-items: center;
-  background-color: ${(props) => props.theme.backgroundColor};
+  background-color: ${(props) => props.theme.bgColor};
 `;
-const rotateAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    border-radius: 100px;
 
-  }
-  100% {
-    transform: rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-const Emoji = styled.span`
-  font-size: 56px;
-`;
-const Box = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  animation: ${rotateAnimation} 1s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 150px;
-    }
-    &:active {
-      opacity: 0;
-    }
-  }
-`;
 function App() {
   return (
     <Wrapper>
--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-const darkTheme = {
-  textColor: "whitesmoke",
-  backgroundColor: "#111",
-};
-const lightTheme = {
-  textColor: "#111",
-  backgroundColor: "whitesmoke",
-};
-
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>
