bc70b7959626817a867063d2b67a7f76e22075d8
rebootedCoder2 <oborimoon@gmail.com>
2024-02-06 09:59:36 +0900

    apply styles

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -3,7 +3,7 @@ import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 
 const Title = styled.h1`
-  font-size: 48px;
+  font-size: 42px;
   color: ${(props) => props.theme.accentColor};
 `;
 const Loader = styled.h3`
@@ -19,6 +19,36 @@ const Header = styled.header`
   align-items: center;
 `;
 
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+
+  span:last-child {
+    font-size: 16px;
+    font-weight: 400;
+  }
+`;
+
+const Description = styled.p`
+  margin: 20px 0px;
+  line-height: 1.2;
+`;
 interface RouteParams {
   coinId: string;
 }
@@ -113,9 +143,41 @@ function Coin() {
   return (
     <Container>
       <Header>
-        <Title>코인 {state?.name || "Loading..."}</Title>
+        <Title>
+          {state?.name ? state.name : loading ? "Loading..." : info?.name}
+        </Title>
       </Header>
-      {loading ? <Loader>Loading...</Loader> : null}
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>{info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>total supply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>max supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+        </>
+      )}
     </Container>
   );
 }
