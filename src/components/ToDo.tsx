e63561f0fd12c7efecce4b7b14987b9853e64439
rebootedCoder2 <oborimoon@gmail.com>
2024-02-10 07:33:47 +0900

    categories

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,33 @@
 import { ITodo } from "./atoms";
 
-function ToDo({ text }: ITodo) {
+function ToDo({ text, category }: ITodo) {
+  // const onClick = (newCategory: ITodo["category"]) => {
+  //   console.log(newCategory);
+  // };
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(event.currentTarget.name);
+    const {
+      currentTarget: { name },
+    } = event;
+  };
   return (
     <li>
       <span>{text}</span>
-      <button>To Do</button>
-      <button>Doing</button>
-      <button>Done</button>
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
     </li>
   );
 }
