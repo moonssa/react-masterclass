a80f746467a8a232cb8ac90eeea14de3c17afad3
rebootedCoder2 <oborimoon@gmail.com>
2024-02-11 07:16:41 +0900

    change doing-done-todo mode when button is clicked

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,14 +1,28 @@
-import { ITodo } from "./atoms";
+import { useSetRecoilState } from "recoil";
+import { ITodo, toDoState } from "./atoms";
 
-function ToDo({ text, category }: ITodo) {
+function ToDo({ text, category, id }: ITodo) {
   // const onClick = (newCategory: ITodo["category"]) => {
   //   console.log(newCategory);
   // };
+  const setToDos = useSetRecoilState(toDoState);
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     console.log(event.currentTarget.name);
     const {
       currentTarget: { name },
     } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      console.log(targetIndex);
+      const oldToDo = oldToDos[targetIndex];
+      const newToDo = { text, id, category: name as any };
+      console.log(oldToDo, newToDo);
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
   };
   return (
     <li>
