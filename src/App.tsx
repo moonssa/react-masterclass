8c12e3ed07b771cb33cb5fffba490eb49d555867
rebootedCoder2 <oborimoon@gmail.com>
2024-02-16 14:47:40 +0900

    source refactoring and 'React.memo'를 이용한 성능향상

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import {
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { toDoState } from "./components/atoms";
+import DraggableCard from "./components/DragabbleCard";
 
 const Wrapper = styled.div`
   display: flex;
@@ -31,12 +32,6 @@ const Board = styled.div`
   border-radius: 5px;
   min-height: 200px;
 `;
-const Card = styled.div`
-  padding: 5px 10px;
-  border-radius: 5px;
-  margin-bottom: 5px;
-  background-color: ${(props) => props.theme.cardColor};
-`;
 
 const toDos = ["a", "b", "c", "d", "e", "done"];
 function App() {
@@ -59,17 +54,7 @@ function App() {
             {(magic) => (
               <Board ref={magic.innerRef} {...magic.droppableProps}>
                 {toDos.map((toDo, index) => (
-                  <Draggable key={toDo} draggableId={toDo} index={index}>
-                    {(magic) => (
-                      <Card
-                        ref={magic.innerRef}
-                        {...magic.draggableProps}
-                        {...magic.dragHandleProps}
-                      >
-                        {toDo}
-                      </Card>
-                    )}
-                  </Draggable>
+                  <DraggableCard key={toDo} index={index} toDo={toDo} />
                 ))}
                 {magic.placeholder}
               </Board>
