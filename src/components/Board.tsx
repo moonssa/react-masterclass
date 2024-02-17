ccbf4801fe6c9a6253449f8727619a6f92557e26
rebootedCoder2 <oborimoon@gmail.com>
2024-02-17 14:22:03 +0900

    ref

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,6 +1,7 @@
 import { Droppable } from "react-beautiful-dnd";
 import DraggableCard from "./DragabbleCard";
 import styled from "styled-components";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   padding: 10px;
@@ -40,9 +41,18 @@ interface IBoardProps {
 }
 
 function Board({ toDos, boardId }: IBoardProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const onClick = () => {
+    inputRef.current?.focus();
+    setTimeout(() => {
+      inputRef.current?.blur();
+    }, 5000);
+  };
   return (
     <Wrapper>
       <Title> {boardId}</Title>
+      <input ref={inputRef} placeholder="Grab me" />
+      <button onClick={onClick}>Click Me</button>
       <Droppable droppableId={boardId}>
         {(magic, snapshot) => (
           <Area
