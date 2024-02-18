51012b42a28fa3dd976fb57586dd4afc22332ffe
rebootedCoder2 <oborimoon@gmail.com>
2024-02-18 12:51:17 +0900

    Animation-Drag

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
@@ -8,33 +9,47 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
+
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-const Circle = styled(motion.div)`
-  background-color: white;
-  width: 70px;
-  height: 70px;
-  place-self: center;
-  border-radius: 35px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const boxVariants = {
   hover: { scale: 2, rotateZ: 45 },
   click: { scale: 1, borderRadius: "100px" },
+  drag: { backgroundColor: "rgb(46,204,113)", transition: { duration: 10 } },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box variants={boxVariants} whileHover="hover" whileTap="click" />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragElastic={0.5}
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileHover="hover"
+          whileDrag="drag"
+          whileTap="click"
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
