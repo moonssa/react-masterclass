f248ec7daebee65e8cf96bba3290222bd13f2e3e
rebootedCoder2 <oborimoon@gmail.com>
2024-02-23 23:09:09 +0900

    slider-1

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
@@ -30,8 +31,11 @@ const BiggerBox = styled.div`
 const Box = styled(motion.div)`
   width: 400px;
   height: 200px;
-  position: absolute;
   top: 100px;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 20px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
@@ -78,22 +82,51 @@ const boxForPreVariants = {
     transition: { duration: 1.5 },
   },
 };
+const boxSlideVariants = {
+  invisible: {
+    x: 500,
+    opacity: 0,
+    scale: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 1,
+    },
+  },
+  exit: {
+    x: -500,
+    opacity: 0,
+    scale: 0,
+    rotateX: 180,
+    transition: {
+      duration: 1,
+    },
+  },
+};
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => setShowing((prev) => !prev);
+  const [showing, setShowing] = useState(1);
+  const nextPlease = () => setShowing((prev) => (prev === 10 ? 10 : prev + 1));
   return (
     <Wrapper>
       <AnimatePresence>
-        {showing ? (
-          <Box
-            variants={boxForPreVariants}
-            initial="initial"
-            animate="visible"
-            exit="leaving"
-          />
-        ) : null}{" "}
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
+          i === showing ? (
+            <Box
+              variants={boxSlideVariants}
+              initial="invisible"
+              animate="visible"
+              exit="exit"
+              key={i}
+            >
+              {i}
+            </Box>
+          ) : null
+        )}
       </AnimatePresence>
-      <button onClick={toggleShowing}>Click</button>
+      <button onClick={nextPlease}>Click</button>
     </Wrapper>
   );
 }
